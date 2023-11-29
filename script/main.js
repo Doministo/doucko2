1b941e73254f3aa6eae0b301fbadc8e53d40cccf
Dominik Štochl <stochld@wxk.zcu.cz>
2023-11-29 21:23:44 +0100

    doucko2
    doma mám něco dodělat

---
+++ script/main.js
@@ -0,0 +1,16 @@
+const seznam = [
+    {Jmeno: "Petr", Prijmeni: "Breich", Rocnik: "1."},
+    {Jmeno: "Vašek", Prijmeni: "Kvěch", Rocnik: "2."}
+
+];
+
+function VypisDoTabulky() 
+{
+    let table = document.getElementById("studentTable");
+}
+
+function Pridat()
+{
+    VypisDoTabulky();
+}
+
